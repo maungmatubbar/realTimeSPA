83266a4f8762aeaf01d89f4ff389def934931bb9
maungmatubbar <maungmatubbar@gmail.com>
2021-06-22 11:38:30 +0600

    Create Relationship Model Question, Reply, User

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,16 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
 class Question extends Model
 {
-    //
+    public function user(){
+        return belongsTo(User::class);
+    }
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
