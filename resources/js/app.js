f5e644ed8e07dfba8d0a6f18a3cd64aab58e2b53
maungmatubbar <maungmatubbar@gmail.com>
2021-08-12 22:44:49 +0600

    Install Vuetify and toolbox implement and VueRouter installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+Vue.component('AppHome', require('./components/AppHome').default)
+
+import router from './Router/router'
+const app = new Vue({
+    el: '#app',
+    router
+});
